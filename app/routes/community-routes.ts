e9361ca34002583ae99e51604399de271ef1d43e
Kartik <kartik3344@gmail.com>
2026-07-15 19:10:32 +0530

    add route for fetch user communities data

--- app/routes/community-routes.ts
+++ app/routes/community-routes.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { communities, communityMembers } from "@/db/schema";
+import { getOrCreateUserByClerkId } from "@/lib/user-utils";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
@@ -8,15 +9,12 @@ type Variables = {
     userId: string;
 }
 
-const communitiesApp = new Hono<{Variables: Variables}>();
-
-communitiesApp
+const communitiesApp = new Hono<{Variables: Variables}>()
     .get("/all", async(c) => {
         const allCommunites = await db.select().from(communities);
 
         return c.json(allCommunites)
     })
-
     .post("/:communityId/join", async(c) => {
         const clerkId = c.get("userId") as string;
         const {communityId} = c.req.param();    
@@ -33,6 +31,28 @@ communitiesApp
         return c.json({
             message: "Joined Community Successfully"
         })
-    });
+    })
+    .get("/", async (c) => {
+        const clerkId = c.get("userId");
+        const user = await getOrCreateUserByClerkId(clerkId);
+        console.log(user)
+        
+        if(!user) return c.json([]);
+        
+        //fetching all all communities the user is part of         
+        const userCommunities = await db.select({
+                                    id:communityMembers.id,
+                                    userId:communityMembers.userId,
+                                    communityId:communityMembers.communityId,
+                                    joinedAt:communityMembers.joinedAt,
+                                    community: communities,
+                            })
+                            .from(communityMembers)
+                            .innerJoin(communities,eq(communityMembers.communityId,communities.id))
+                            .where(eq(communityMembers.userId,user.id));
+                console.log(userCommunities)
+        
+        return c.json(userCommunities);
+    })
 
-export default communitiesApp ;
+export {communitiesApp} ;
