76366ab0a203dfd8539b8feb015f23da3d6d8af6
Kartik <kartik3344@gmail.com>
2026-07-17 22:36:44 +0530

    add useJoinCommunity hook to update user communities and add useAllCommunities to fetch data of all communities

--- hooks/useCommunities.ts
+++ hooks/useCommunities.ts
@@ -1,5 +1,5 @@
 import { client } from "@/lib/api-client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export function useCommunities(){
     return useQuery({
@@ -10,6 +10,17 @@ export function useCommunities(){
             return res.json()
         }
     }) 
+}
+
+export function useAllCommunities(){
+    return useQuery({
+        queryKey: ["allCommunities"],
+        queryFn: async () => {
+            const res = await client.api.communities.all.$get()
+            if(!res.ok) throw new Error("Failed to fetch all communities");
+            return res.json()
+        }
+    })
 }
 
 export function useCommunityGoals(communityId: string){
@@ -24,5 +35,25 @@ export function useCommunityGoals(communityId: string){
             return res.json()
         }, 
         enabled: !!communityId
+    })
+}
+
+export function useJoinCommunity(){
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (communityId:string) => {
+            const res = await client.api.communities[":communityId"].join.$post({
+                param: { communityId }
+            });
+
+            if(!res.ok) throw new Error("Failed to join community");
+            return res.json();
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["communities"] })
+        }, 
+        onError: (error) => {
+            console.log("error joining community", error)
+        }
     })
 }
