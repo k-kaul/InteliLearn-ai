31ec9845f1da336284c35bf9307b4efa7ad47c16
Kartik <kartik3344@gmail.com>
2026-08-07 20:24:44 +0530

    update matching component UI, added Ai matching handler

--- components/communities/ai-matching.tsx
+++ components/communities/ai-matching.tsx
@@ -1,6 +1,26 @@
+import { useAiPartners } from "@/hooks/use-ai-partner";
 import { Button } from "../ui/button";
+import { toast } from "../ui/toast";
 
-export default function AIMatching({ totalGoals }: { totalGoals:number }){
+export default function AIMatching({ totalGoals, selectedCommunityId }: { totalGoals:number; selectedCommunityId:string; }){
+    const aiPartnerMutation = useAiPartners();
+
+    async function handleFindAiPartners(){
+        try {
+            await aiPartnerMutation.mutateAsync(selectedCommunityId);
+            toast.add({
+                title: "AI partners found successfully",
+                type: "success"
+            })
+        } catch (error) {
+            console.error("Error finding AI partners", error);
+            toast.add({
+                title: "Error finding AI partners",
+                type: "fail"
+            })
+        }
+    }
+
     return (
         <div className="text-center py-8">
             <div className="mb-3">
@@ -9,7 +29,10 @@ export default function AIMatching({ totalGoals }: { totalGoals:number }){
                     Our AI will analyze your learning goals and automatically match you with the most compatible learning partners in this community.
                 </p>
             </div>
-            <Button size={"lg"} disabled={totalGoals === 0}>Find Partners with AI</Button>
+            <Button size={"lg"} 
+                disabled={totalGoals === 0}
+                onClick={handleFindAiPartners}
+                >Find Partners with AI</Button>
             {
                 totalGoals > 0 ? (
                     <p className="mt-4 text-sm text-muted-foreground">{`You have ${totalGoals} learning${totalGoals === 1 ? " goal" : " goals"} set`}</p>
