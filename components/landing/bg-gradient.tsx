073414da02156583c0b5a3314b5da69511b34005
Kartik <kartik3344@gmail.com>
2026-08-14 18:29:10 +0530

    remmoved unused files

--- components/landing/bg-gradient.tsx
+++
@@ -1,9 +0,0 @@
-import { BackgroundBeamsWithCollision } from "../ui/background-beams-with-collision";
-
-export default function BackgroundGradient(){
-    return (
-            <BackgroundBeamsWithCollision className="fixed inset-0 -z-10 overflow-hidden">
-                <></>
-            </BackgroundBeamsWithCollision>
-    )
-}
